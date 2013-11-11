2893bc388ec66a37cc0bfce21c95ba0d25489b9b
Charles Guo <shaldengeki@gmail.com>
2013-11-11 14:12:32 -0600

    queue now takes kwargs instead of a row

--- DbConn/DbInsertQueue.py
+++ DbConn/DbInsertQueue.py
@@ -6,10 +6,15 @@ class DbInsertQueue(object):
   '''
   Simple database class to insert large volumes of entries into a table.
   '''
-  def __init__(self, dbConn, table, fields, maxLength=1000):
+  def __init__(self, dbConn, table, fields, maxLength=5000):
     self.db = dbConn
-    self.table(table).fields(fields).clear().maxLength(maxLength).ignore(False).update(None)
+    self.table(table).fields(*fields).clear().maxLength(maxLength).ignore(False).update(None)
 
+  def __str__(self):
+    return "DbInsertQueue on table " + str(self._table) + " for fields " + str(self._fields) + " with maximum length " + str(self._maxLength)
+
+  def __unicode__(self):
+    return u"DbInsertQueue on table " + unicode(self._table) + u" for fields " + unicode(self._fields) + u" with maximum length " + unicode(self._maxLength)
 
   def __len__(self):
     return int(self._length)
@@ -27,10 +32,10 @@ class DbInsertQueue(object):
     self._table = unicode(table)
     return self
 
-  def fields(self, fields):
+  def fields(self, *args):
     # sets fields.
-    self._fields = list(fields)
-    self._numFields = len(fields)
+    self._fields = args
+    self._numFields = len(args)
     return self
 
   def maxLength(self, maxLength):
@@ -45,9 +50,17 @@ class DbInsertQueue(object):
     self._update = update
     return self
 
-  def queue(self, row):
+  def queue(self, **kwargs):
     if len(row) != self._numFields:
       raise DbInsertQueueException(u"Row field count mismatch: trying to queue " + unicode(len(row)) + u" values into " + unicode(self._numFields) + u" fields")
+
+    new_row = []
+    for idx,field in enumerate(self._fields):
+      if field not in kwargs:
+        raise DbInsertQueueException(u"Row field mismatch: " + unicode(field) + " not found in queued row")
+      new_row.append((idx, field))
+
+    new_row = [x[1] for x in sorted(new_row, key=lambda x: x[0])]
     self._rows.append(row)
     self._length += 1
     if self._length > self._maxLength:
